3727f913b67f3d12857dbef49f5660fd9873b65e
蓝天 <eyjian@qq.com>
2024-09-16 11:49:18 +0800

    Update main.go

--- main.go
+++ main.go
@@ -43,7 +43,8 @@ func main() {
 				return err
 			}
 			if d.Type().IsDir() {
-				leafDir := filepath.Base(filepath.Dir(path))
+				tmpDir := filepath.Join(path, "xyz###123")
+				leafDir := filepath.Base(filepath.Dir(tmpDir))
 				if IsValidYYYYMMDD(leafDir) {
 					fmt.Fprintf(os.Stderr, "Directory `%s` is skipped\n", path)
 					return filepath.SkipDir
